1b3a58b7ad5d31d837f9899cbb61c07f518cb677
Azril <100113789+MAzrilFahrezi@users.noreply.github.com>
2022-09-23 11:19:47 +0700

    update Fuzzy2

--- Fuzzy2.m
+++ Fuzzy2.m
@@ -19,6 +19,31 @@ Iy = conv2(I,Gy,'same');
 figure; image(Ix,'CDataMapping','scaled'); colormap('gray'); title('Ix');
 figure; image(Iy,'CDataMapping','scaled'); colormap('gray'); title('Iy');
 
+edgeFis=newfis('edgeDetection');
+
+edgeFis = addvar(edgeFis,'input','Ix',[-1 1]);
+edgeFis = addvar(edgeFis,'input','Iy',[-1,1]);
+
+sx = 0.1; sy = 0.1;
+edgeFis = addmf(edgeFis,'input',1,'zero','gaussmf',[sx 0]);
+edgeFis = addmf(edgeFis,'input',2,'zero','gaussmf',[sy 0]);
+
+edgeFis = addvar(edgeFis,'output','Iout',[0 1]);
+
+wa = 0.1; wb = 1; wc = 1;
+ba = 0; bb = 0; bc = .7;
+edgeFis = addmf(edgeFis,'output', 1,'white','trimf',[wa wb wc]);
+edgeFis = addmf(edgeFis,'output', 1,'black','trimf',[wa wb wc]);
+
+figure
+subplot(2,2,1);plotmf(edgeFis,'input',1); title(Ix);
+subplot(2,2,2);plotmf(edgeFis,'input',2); title(Iy);
+subplot(2,2,[3 4]);plotmf(edgeFis,'output',1); title('Iout');
+
+
+
+
+
 
 
 
